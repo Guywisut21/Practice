348901633762b425ef083981a292f56cdf1811df
Guywisut21 <guywisut21@hotmail.com>
2023-04-17 19:24:55 +0700

    python EP6 about function input

---
+++ python_study/EP6.py
@@ -0,0 +1,15 @@
+# funcion input str
+name=input("Please type your name : ")
+print("You type = "+name)
+print(type(name))
+
+fname=input("Please type your first name : ")
+lname=input("Please type your last name : ")
+print("Your full name : "+fname)
+print("Your last name : "+lname)
+
+# funtion input int,float
+x=int(input("Num 1 : "))
+y=int(input("Num 2 : "))
+
+print(x+y)
