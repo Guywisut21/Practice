78ae763adad6ca806a755ddf94f7f40aa13802b8
Guywisut21 <guywisut21@hotmail.com>
2023-04-17 19:26:33 +0700

    python EP7 about operator

---
+++ python_study/EP7.py
@@ -0,0 +1,10 @@
+# operator
+x=10
+y=5
+print("ผลบวก" , x+y)
+print("ผลลบ" , x-y)
+print("ผลคูณ" , x*y)
+print("ผลหาร" , x/y)
+print("ผลหารปัดเศษ" , x//y)
+print("ผลหารเก็บเศษ" , x%y)
+print("เลขยกกำลัง" , x**y)
