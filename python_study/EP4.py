91f06551ed9525579e96aba6f6035f78d2bf5d12
Guywisut21 <guywisut21@hotmail.com>
2023-04-17 19:21:29 +0700

    python EP4 about rules of name

---
+++ python_study/EP4.py
@@ -0,0 +1,19 @@
+#กฎการตั้งชื่อ
+
+"""
+1.ใส่ตัวเลข ตัวอักษร เครื่องหมาย
+2.ห้ามขึ้นต้นด้วยตัวเลขและสัญลักษณ์ ยกเว้น_
+3.ห้ามซ้ำคีย์เวิร์ด เช่น False class finally is return None continue for lambda try True def from nonlocal while and del global not with as elif if or yield assert else import pass break except in raise
+4.case sensitive
+"""
+a=10
+_a="Guywisut"
+_if="Star"
+
+name="A"
+NAME="B"
+nAmE="C"
+
+print(name)
+print(NAME)
+print(nAmE)
